8325f526876b4b8021ed3b551a1353eff03726a7
robertfeliciano <robertfeliciano314@gmail.com>
2024-04-27 19:56:46 -0400

    implemented run cell and timing

--- client/src/components/Notebook.jsx
+++ client/src/components/Notebook.jsx
@@ -1,31 +1,91 @@
 import {useParams} from "react-router-dom";
 import {useMutation, useQuery} from "@apollo/client";
-import {GETNB} from "../queries";
-import {useState, useEffect, useRef} from "react";
+import {GETNB, QUICKDATA, UPDATENB, USERNBS} from "../queries";
+import {useState, useEffect, useRef, useContext} from "react";
 import { io } from 'socket.io-client';
-import TextareaAutosize from "react-textarea-autosize";
-
+import {Delete} from './Delete';
+import CircularProgress from "@mui/joy/CircularProgress";
+import {AuthContext} from './AuthContext';
+import Textarea from '@mui/joy/Textarea';
+import Input from '@mui/joy/Input';
 
 export const Notebook = () => {
   const {id} = useParams();
   const [errMsg, setErrMsg] = useState('');
+  const [completed, setCompleted] = useState(true);
+  const tick = useRef(0);
+  const socketRef = useRef();
   const {loading, error, data} = useQuery(GETNB, {
     variables: {id},
     onError: (e) => setErrMsg(e.message),
     fetchPolicy: 'cache-first'
   });
-
-  const socketRef = useRef();
-
   useEffect(() => {
+    // set up socket endpoint stuff
     const endpt =  import.meta.env.VITE_KERNEL_ENDPOINT;
     socketRef.current = io(endpt);
 
-    socketRef.current.on('output', (({output}) => console.log(output)));
+    socketRef.current.on('output', (({output}) => {
+      const tock = performance.now();
+      console.log(output)
+    }));
     return () => {
       socketRef.current.disconnect();
     };
   }, []);
+
+  const {currentUser} = useContext(AuthContext);
+  const [saveNB] = useMutation(UPDATENB, {
+    onError: (e) => setErrMsg(e.message),
+    onCompleted: () => setCompleted(true),
+    update(cache, { data: { updateNotebook } }) {
+      const {getUserNotebooks} = cache.readQuery({query: USERNBS}) || {};
+      if (getUserNotebooks) {
+        cache.writeQuery({
+          query: USERNBS,
+          data: {
+            getUserNotebooks:
+              getUserNotebooks.map(nb => nb._id === updateNotebook._id ?
+                {
+                  _id: updateNotebook._id,
+                  name: updateNotebook.name,
+                  date: updateNotebook.date
+                } : nb)
+          }
+        });
+      }
+      const {getQuickDataFromUser} = cache.readQuery({query: QUICKDATA}) || {};
+      if (getQuickDataFromUser) {
+        cache.writeQuery({
+          query: QUICKDATA,
+          data: {
+            getQuickDataFromUser:
+              getQuickDataFromUser.map(qd => qd._id === updateNotebook._id ?
+                {
+                  _id: updateNotebook._id,
+                  name: updateNotebook.name,
+                  type: 'notebook',
+                  date: updateNotebook.date
+                } : qd)
+          }
+        });
+      }
+      const {getNotebookById} = cache.readQuery({query: GETNB, variables: {id: updateNotebook._id}}) || {};
+      if (getNotebookById) {
+        cache.writeQuery({
+          query: GETNB,
+          data: {
+            getNotebookById: {
+              _id: updateNotebook._id,
+              date: updateNotebook.date,
+              name: updateNotebook.name,
+              pairs: updateNotebook.pairs
+            }
+          }
+        });
+      }
+    }
+  });
 
   if (loading || !socketRef.current)
     return (
@@ -33,6 +93,7 @@ export const Notebook = () => {
         Loading...
       </div>
     );
+  // TODO add errormodal popup or snackbar
   if (errMsg || error)
     return (
       <div>
@@ -41,33 +102,73 @@ export const Notebook = () => {
     )
 
   // TODO add time display to see how long cell takes to run using performance.now()
-  const runCell = () => {
-
+  const runCell = (idx) => {
+    let cell = document.getElementById(`lala-input-${idx}`)?.value;
+    cell = cell.trim();
+    if (!cell || cell === ''){
+      console.log('ERROR CHECK -> cell must contain valid lala')
+    }
+    tick.current = performance.now();
+    socketRef.current.emit('run', {cell_text:cell, auth: currentUser.uid});
   }
 
-
-  const pingServer = () => {
-    console.log('attempting to ping server');
-    socketRef.current.emit('run', {cell_text:"let a = 1 0 0 ; 0 1 0 ; 0 0 1", auth: "123"});
-  };
-  pingServer();
+  const onClickSave = (e) => {
+    e.preventDefault();
+    setCompleted(false);
+    const name = document.getElementById('nb-name')?.value;
+    const pairs = [];
+    let idx = 0;
+    let input = document.getElementById(`lala-input-${idx}`)?.value;
+    let output = document.getElementById(`lala-output-${idx}`)?.innerText;
+    while (input){
+      idx += 1;
+      pairs.push( { input, output } );
+      input = document.getElementById(`lala-input-${idx}`)?.value;
+      output = document.getElementById(`lala-output-${idx}`)?.innerText;
+    }
+    const id = nb._id;
+    const date = new Date();
+    const variables = {name, id, pairs, date};
+    saveNB({variables});
+  }
 
   const nb = data?.getNotebookById;
   const cells = nb?.pairs;
   return (<>
     <br/>
-    <div className={'file-options'}>
-      <button style={{marginRight: '0.5rem'}}>
-        Save Document
+    <div className={'file-options'} style={{zIndex: 2}}>
+      <button style={{marginRight: '0.5rem'}} onClick={onClickSave}>
+        {completed ? "Save Notebook" : <CircularProgress variant={'soft'} color={'neutral'} thickness={1}/>}
       </button>
-      <button style={{marginLeft: '0.5rem'}}>
-        Delete Document
-      </button>
+      <Delete
+        type={'Notebook'}
+        name={nb.name}
+        id={nb._id}
+      />
     </div>
-    <div>
-      <h1>
-        {nb.name}
-      </h1>
+    <div align={'center'} style={{marginTop: '5rem', zIndex: 1}}>
+      <Input
+        defaultValue={nb.name}
+        autoComplete={'off'}
+        style={{
+          height: '100%',
+          backgroundColor: 'black',
+          color: 'white',
+          width: `${nb.name.length + 3}rem`,
+        }}
+        sx={{
+          '--Input-focusedInset': 'var(--any, )',
+          '--Input-focusedThickness': '0.2rem',
+          '--Input-focusedHighlight': 'rgba(13,110,253,.25)',
+          '&::before': {
+            transition: 'box-shadow .15s ease-in-out',
+          },
+          '&:focus-within': {
+            borderColor: '#86b7fe',
+          },
+        }}
+        slotProps={{input: {id: 'nb-name'}}}
+      />
       <cite>
         {nb.date}
       </cite>
@@ -79,17 +180,36 @@ export const Notebook = () => {
       cells.map(({input, output}, idx) => {
         return (
           <div key={idx}>
-            <div>
-              <TextareaAutosize
+            <div align={'center'}>
+              <Textarea
                 defaultValue={input}
                 minRows={6}
                 aria-label={`input-${idx}`}
-                style={{width: 500}}
+                style={{
+                  height: '100%',
+                  backgroundColor: 'black',
+                  color: 'white',
+                  width: '40rem',
+                  marginBottom: '1rem'
+                }}
+                sx={{
+                  '--Textarea-focusedInset': 'var(--any, )',
+                  '--Textarea-focusedThickness': '0.2rem',
+                  '--Textarea-focusedHighlight': 'rgba(13,110,253,.25)',
+                  '&::before': {
+                    transition: 'box-shadow .15s ease-in-out',
+                  },
+                  '&:focus-within': {
+                    borderColor: '#86b7fe',
+                  },
+                }}
+                slotProps={{textarea: {id: `lala-input-${idx}`}}}
               />
-              <button onClick={runCell}>Run Cell</button>
+              <button
+                style={{marginBottom: '1rem'}}
+                onClick={() => runCell(idx)}>Run Cell</button>
             </div>
-            <div>
-              <label>Output: </label>
+            <div id={`lala-output-${idx}`}>
               {output}
             </div>
             <br/>
@@ -98,6 +218,5 @@ export const Notebook = () => {
         )
       })
     }
-
   </>)
 }
