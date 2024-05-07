184ae02ddfb081ecd8cfc6cc666479f982491a70
robertfeliciano <robertfeliciano314@gmail.com>
2024-05-07 13:42:24 -0400

    seed test user

--- server/tasks/seed.js
+++ server/tasks/seed.js
@@ -7,7 +7,7 @@ const main = async () => {
 
     const users = await col.users();
     let newUser = await users.insertOne({
-        firebaseId: "a2JTGDDkBPSJjHCYMTt2KsdMgAD3",
+        firebaseId: "yjr3nW5daGO4QOkZBVvGnEoujTI2",
         notebooks: [],
         documents: []
     });
@@ -19,24 +19,45 @@ const main = async () => {
 
     const newDoc = await docs.insertOne({
         ownerFid: newUser.firebaseId,
-        file: "i am a new doc",
-        name: "document 1",
+        file: `
+        let a = 1 0 0 ; 0 1 0 ; 0 0 1
+        let b = 1 2 3 ; 4 5 6 ; 7 8 9
+        let c = a @ b
+        // c should equal b
+        // note that this matrix does not have an inverse !
+        c
+        `,
+        name: "dot products!",
         date: new Date('12/19/2023')
     });
     const newDoc2 = await docs.insertOne({
         ownerFid: newUser.firebaseId,
-        file: "i am another new doc",
-        name: "document 2",
+        file: `
+          fun myFunction = (a b) => {
+            let c = a @ b    // dot product
+            let d = % c    // transpose
+            let e = (? d) ** (2 1 3 ; 9 1 4 ; 8 1 7)    // inverse and elem-wise multiplication
+            e
+          }
+          
+          /* 
+          wrapper is going to return the function myFunction
+          */  
+          fun wrapper = () => {
+            myFunction
+          }
+          
+          let m = 1 0 0 ; 0 1 0 ; 0 0 1
+          let n = 1 2 3 ; 4 5 6 ; 7 8 10
+          
+          let anon = wrapper()
+          let z = anon(m n)
+          z`,
+        name: "first class functions",
         date: new Date('4/5/2023')
     });
-    const newDoc3 = await docs.insertOne({
-        ownerFid: newUser.firebaseId,
-        file: "i am a third new doc",
-        name: "document 3",
-        date: new Date('8/1/2023')
-    });
 
-    for (const doc of [newDoc, newDoc2, newDoc3]) {
+    for (const doc of [newDoc, newDoc2]) {
         await users.findOneAndUpdate(
           { _id: newUser._id },
           { $push: { documents: doc.insertedId } },
@@ -46,39 +67,39 @@ const main = async () => {
 
     const newNB = await nbs.insertOne({
         ownerFid: newUser.firebaseId,
-        pairs: [{ input: 'input', output: 'output' }],
+        pairs: [{ input: 'let a = 1 0 0 ; 0 1 0 ; 0 0 1', output: '' }],
         name: "notebook 1",
         date: new Date('3/22/2024')
     });
     const newNB2 = await nbs.insertOne({
         ownerFid: newUser.firebaseId,
-        pairs: [{ input: 'input2', output: 'output2' }],
+        pairs: [{ input: `
+        fun coolFun = (p1 p2) => {
+          let tmp = p1 @ p2
+          let tmp = % tmp
+          tmp
+        }
+        
+        let a = 1 2 ; 3 4 
+        let b = 3 4 ; 1 2
+        let c = coolFun(a b)
+        c
+        `, output: '' }],
         name: "notebook 2",
         date: new Date('1/4/2024')
-    });
-    const newNB3 = await nbs.insertOne({
-        ownerFid: newUser.firebaseId,
-        pairs: [{ input: 'input3', output: 'output3' }],
-        name: "notebook 3",
-        date: new Date('9/05/2023')
     });
     const bigNB = await nbs.insertOne({
         ownerFid: newUser.firebaseId,
         pairs: [
-            { input: 'let a = 1 2 ; 3 4\na', output: '1 2 ; 3 4' },
-            { input: '/dbg', output: 'a := 12 ; 34' },
-            { input: '//hmmmmm', output: '' },
-            { input: 'let b = ident(1)\nb', output: '1 0 ; 0 1' },
-            { input: 'b@a', output: '1 2 ; 3 4' },
-            { input: '//vectors..', output: 'yess...' },
-            { input: 'almost done', output: 'output' },
-            { input: 'finally', output: 'yes!' },
+            { input: 'let a = 1 2 ; 3 4\na', output: '' },
+            { input: 'let b = 5 6 ; 7 8', output: '' },
+            { input: 'let c = a @ b', output: '' },
         ],
         name: "big notebook",
         date: new Date()
     })
 
-    for (const nb of [newNB, newNB2, newNB3, bigNB]) {
+    for (const nb of [newNB, newNB2, bigNB]) {
         await users.findOneAndUpdate(
           { _id: newUser._id },
           { $push: { notebooks: nb.insertedId } },
